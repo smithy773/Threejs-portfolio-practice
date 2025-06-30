6e6abd567d1d13fda30e55a49e30b6c0b7b28fb5
Viktor Kolev <viktorkolev0707@gmail.com>
2025-06-30 21:53:11 +0300

    Add FeatureCards

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import NavBar from "./components/NavBar";
+import FeatureCards from "./sections/FeatureCards";
 import Hero from "./sections/Hero";
 import ShowcaseSection from "./sections/ShowcaseSection";
 
@@ -8,6 +9,7 @@ export default function App() {
       <NavBar />
       <Hero />
       <ShowcaseSection />
+      <FeatureCards />
     </>
   );
 }
---
+++ src/sections/Experience.tsx
@@ -0,0 +1,14 @@
+import TitleHeader from "../components/TitleHeader";
+
+export default function Experience() {
+  return (
+    <section
+      id="experience"
+      className="w-full md:mt-40 mt-20 section-padding xl:px-0"
+    >
+      <div className="w-full h-full md:px-20 px-5">
+        <TitleHeader title="Programming Experience" sub="My Coding Journey" />
+      </div>
+    </section>
+  );
+}
